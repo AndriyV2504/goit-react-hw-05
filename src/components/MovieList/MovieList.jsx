0e32fb4cfcabf63b820446507314be38e1521c43
AndriyV2504 <andreyvolyanik@gmail.com>
2024-07-26 22:11:33 +0300

    MovieList Link

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const MovieList = ({ movies }) => {
+  return (
+    <div>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MovieList;
