e99c6fd07ab757d18d686445fb792276b9e7d917
AndriyV2504 <andreyvolyanik@gmail.com>
2024-07-27 13:30:24 +0300

    MovieCast map

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -3,14 +3,48 @@ import { useParams } from "react-router-dom";
 import { fetchMovieCast } from "../../services/api";
 
 const MovieCast = () => {
-  const params = useParams();
+  const { movieId } = useParams();
   const [cast, setCast] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   fetchMovieCast(params.movieId).then((data) => setCast(data));
+  // }, [params.movieId]);
 
   useEffect(() => {
-    fetchMovieCast(params.movieId).then((data) => setCast(data));
-  }, [params.movieId]);
+    const getCast = async () => {
+      try {
+        const data = await fetchMovieCast(movieId);
+        setCast(data.cast);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    getCast();
+  }, [movieId]);
 
-  return <div></div>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading cast: {error.message}</p>;
+
+  return (
+    <div>
+      <ul>
+        {cast.map((actor) => (
+          <li key={actor.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
+              alt={actor.name}
+            />
+            <p>{actor.name}</p>
+            <p>Character: {actor.character}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MovieCast;
